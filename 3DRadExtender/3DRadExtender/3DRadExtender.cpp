5e2d7c3c0a8e38f4728fb405206ee23b9f3c4565
Athanatos <3dradspace@gmail.com>
2019-06-24 22:57:37 +0300

    Added MSGBOX()
    Credits to SLC for helping me with the string buffer parsing issues.

--- 3DRadExtender/3DRadExtender/3DRadExtender.cpp
+++ 3DRadExtender/3DRadExtender/3DRadExtender.cpp
@@ -8,3 +8,32 @@ void DLLEXPORT MyFunction(float *args)
 {
 	args[2] = args[0] + args[1];
 }
+void DLLEXPORT MSGBOX(float *args)
+{
+	char *message = new char[255];
+	char *title = new char[255];
+	int i;
+	for (i =0 ; i < 255; i++)
+	{
+		if ((int)args[i]) message[i] = (int)args[i];
+		else
+		{
+			message[i] = 0;
+			break;
+		}
+	}
+	int j = 0;
+	i += 1;
+	for (; i < 512; i++, j++)
+	{
+		if ((int)args[i]) title[j] = (int)args[i];
+		else
+		{
+			title[j] = 0;
+			break;
+		}
+	}
+	MessageBoxA(nullptr, message, title, MB_OK + MB_ICONINFORMATION);
+	delete[] message;
+	delete[] title;
+}
--- 3DRadExtender/3DRadExtender/3DRadExtender.h
+++ 3DRadExtender/3DRadExtender/3DRadExtender.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <Windows.h>
+#include <fstream>
 #define DLLEXPORT __declspec(dllexport)
 extern "C"
 {
 	void DLLEXPORT MyFunction(float *args);
+	void DLLEXPORT MSGBOX(float *args);
 }
